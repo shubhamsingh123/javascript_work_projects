401920435bb75303491d36e9ca9112ab563055bd
Shubham Singh <Shubham16cse06@gmail.com>
2021-04-11 16:43:47 +0530

    using the console in javascript

---
+++ app.js
@@ -0,0 +1,34 @@
+// log to console
+
+console.log("Hello World");
+
+console.log(123); // number are blue
+
+console.log(true);
+
+const greeting = "Hello";
+console.log(greeting);
+
+console.log([1, 2, 3, 4]);
+
+console.log({ a: 1, b: 2, c: 3 });
+console.table({ a: 1, b: 2, c: 3 });
+
+console.error("This is an error");
+
+console.clear();
+
+console.warn("This is a warning");
+
+console.time("Hello");
+
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+
+console.timeEnd("Hello");
